8a2c54135273924c17104260a7bf47b60f48687c
AaliyahHcode <94814024+AaliyahHcode@users.noreply.github.com>
2026-09-09 02:41:32 -0400

    Created Barriers, Issue with Power Up
    When the power up duration ends, the player starts to float in the space (just needed to freeze the rotation in player contraints rigidbody 2d). Aded pulse to door, hearts to player UI.

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public float pulseSpeed = 2.5f;
+    public float pulsequantity = 0.5f; //how much pulsating (big to small)
+    private Vector3 originalscale; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        originalscale = transform.localScale; //save initial
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pulse = 1.5f + Mathf.Sin(Time.time * pulseSpeed) * pulsequantity; //from stack overflow (gentle pulse)
+        transform.localScale = originalscale * pulse;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManagerMines.Instance.Win();
+        }
+    }
+}
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class PowerUp : MonoBehaviour
 {
+    public float durationofpowerup = 3f; //how long powerup lasts
+    public float increaseSpeed = 3f; //add this much speed
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) //is the object that's touching the powerup, the player?
+        {
+            PlayerControls player = other.GetComponent<PlayerControls>();
+
+            if (player != null)
+            {
+                player.StartCoroutine(SpeedBooster(player)); //start temp. speed booster
+            }
+            Destroy(gameObject); //remove power up after collection
+        }
+    }
+
+    IEnumerator SpeedBooster(PlayerControls player)
+    {
+        float originalspeed = player.movespeed;
+        player.movespeed += increaseSpeed;
+        yield return new WaitForSeconds(durationofpowerup);
+        player.movespeed = originalspeed;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
